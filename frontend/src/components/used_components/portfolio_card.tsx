04345de5e62bbe2c4829b7715fe97b86873baa51
zatan <zatan@LAPTOP-0M1S0F48.localdomain>
2024-01-12 22:02:15 +0000

    setup prettier with tailwindcss

--- frontend/src/components/used_components/portfolio_card.tsx
+++ frontend/src/components/used_components/portfolio_card.tsx
@@ -1,51 +1,47 @@
-import { BellRing, Check } from "lucide-react"
- 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { BellRing, Check } from 'lucide-react'
+
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
 import {
   Card,
   CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Switch } from "@/components/ui/switch"
- 
+  CardTitle
+} from '@/components/ui/card'
+import { Switch } from '@/components/ui/switch'
+
 const notifications = [
   {
-    title: "Your call has been confirmed.",
-    description: "1 hour ago",
+    title: 'Your call has been confirmed.',
+    description: '1 hour ago'
   },
   {
-    title: "You have a new message!",
-    description: "1 hour ago",
+    title: 'You have a new message!',
+    description: '1 hour ago'
   },
   {
-    title: "Your subscription is expiring soon!",
-    description: "2 hours ago",
-  },
+    title: 'Your subscription is expiring soon!',
+    description: '2 hours ago'
+  }
 ]
- 
+
 type CardProps = React.ComponentProps<typeof Card>
- 
+
 export function PortfolioCard({ className, ...props }: CardProps) {
   return (
-    <Card className={cn("w-[380px]", className)} {...props}>
+    <Card className={cn('w-[380px]', className)} {...props}>
       <CardHeader>
-        <CardTitle>Notifications</CardTitle>
+        <CardTitle>PayPal</CardTitle>
         <CardDescription>You have 3 unread messages.</CardDescription>
       </CardHeader>
       <CardContent className="grid gap-4">
         <div className=" flex items-center space-x-4 rounded-md border p-4">
           <BellRing />
           <div className="flex-1 space-y-1">
-            <p className="text-sm font-medium leading-none">
-              Push Notifications
-            </p>
-            <p className="text-sm text-muted-foreground">
-              Send notifications to device.
-            </p>
+            <p className="text-sm font-medium leading-none">Push Notifications</p>
+            <p className="text-sm text-muted-foreground">Send notifications to device.</p>
           </div>
           <Switch />
         </div>
@@ -57,12 +53,8 @@ export function PortfolioCard({ className, ...props }: CardProps) {
             >
               <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
               <div className="space-y-1">
-                <p className="text-sm font-medium leading-none">
-                  {notification.title}
-                </p>
-                <p className="text-sm text-muted-foreground">
-                  {notification.description}
-                </p>
+                <p className=" text-sm font-medium leading-none">{notification.title}</p>
+                <p className="text-sm text-muted-foreground">{notification.description}</p>
               </div>
             </div>
           ))}
