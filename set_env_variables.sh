db1e5ee5424f0c6ef3bda6c2918124412617960a
zatan <zatan@LAPTOP-0M1S0F48.localdomain>
2024-01-12 19:15:20 +0000

    setup shadcn, tailwind and vite

---
+++ set_env_variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export RAILWAY_DATABASE_URL="postgresql://postgres:Cegbef*4gG3--B64aaBa-6BbFEdBG1C1@monorail.proxy.rlwy.net:20304/railway"
+export MYSQL_URL="mysql://zatan:abc123@127.0.0.1:3306"
+export DATABASE_NAME="earnings"
+export RAILWAY_PROD_DATABASE_URL="postgresql://postgres:Cegbef*4gG3--B64aaBa-6BbFEdBG1C1@roundhouse.proxy.rlwy.net:18552/railway"
+
+
+
